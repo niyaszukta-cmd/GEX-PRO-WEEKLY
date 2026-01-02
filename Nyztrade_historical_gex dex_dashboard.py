78c7da37fca25cfd1ed2ebb6ce331b28eb6b2e93
Dr Niyas N (nyztrade) <niyaszukta@gmail.com>
2026-01-03 00:58:56 +0530

    Update Nyztrade_historical_gex dex_dashboard.py

--- Nyztrade_historical_gex dex_dashboard.py
+++ Nyztrade_historical_gex dex_dashboard.py
@@ -198,7 +198,7 @@ st.markdown("""
 @dataclass
 class DhanConfig:
     client_id: str = "1100480354"
-    access_token: str ="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9eyJpc3MiOiJkaGFuIiwicGFydG5lcklkIjoiIiwiZXhwIjoxNzY3NDY2OTQ2LCJhcHBfaWQiOiJjOTNkM2UwOSIsImlhdCI6MTc2NzM4MDU0NiwidG9rZW5Db25zdW1lclR5cGUiOiJBUFAiLCJ3ZWJob29rVXJsIjoiIiwiZGhhbkNsaWVudElkIjoiMTEwMDQ4MDM1NCJ9111DCarHUW03EY_LeZWNb1NkJ8brqmv3HxJMSedwNGJXAf8ob9qXKO_ysLiNaQ0uKS7vBetDHMmdoU7nbzzAtg"
+    access_token: str = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJkaGFuIiwicGFydG5lcklkIjoiIiwiZXhwIjoxNzY3NDY4NDY5LCJhcHBfaWQiOiJjOTNkM2UwOSIsImlhdCI6MTc2NzM4MjA2OSwidG9rZW5Db25zdW1lclR5cGUiOiJBUFAiLCJ3ZWJob29rVXJsIjoiIiwiZGhhbkNsaWVudElkIjoiMTEwMDQ4MDM1NCJ9.65bGEyxwWYyqE33B3LSZQWxBuAGLZ_wLWyn0SNUKPpx0tY1cgOhOoORjpl7edAn_qk7qqgFueN97NT3gLJgCtQ"
 DHAN_SECURITY_IDS = {
     "NIFTY": 13, 
     "BANKNIFTY": 25, 
